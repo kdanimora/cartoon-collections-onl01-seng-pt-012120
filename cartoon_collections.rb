5af9bde021123f192664f98ac50f7f6476660e91
Karen Daniela <kdaniimora@gmail.com>
2020-01-28 17:28:41 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(each_with_index)
+["Doc", "Dopey", "Bashful", "Grumpy"].each_with_index
+end
+
+
 end
 
 def summon_captain_planet# code an argument here
